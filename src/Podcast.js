1bf6600bb2ec802c3a45fd58ffdcb6759a18d722
taufik <taufik@taufiks-MacBook-Pro.local>
2020-04-04 11:41:53 +0700

    add static for title & add episode id label

--- src/Podcast.js
+++ src/Podcast.js
@@ -3,14 +3,22 @@ import Header from './components/Header';
 import Content from './components/Content';
 import Footer from './components/Footer';
 import { DataProvider } from './DataProvider';
+import { Head } from "react-static";
+import logo from "./icons/logo.svg";
 
 const PodcastApp = () => {
   return (
-    <DataProvider>
-      <Header />
-      <Content />
-      <Footer />
-    </DataProvider>
+    <>
+      <Head>
+          <title>Developer Talks</title>
+          <link rel="icon" type="image/png" href={logo} sizes="16x16" />
+      </Head>
+      <DataProvider>
+        <Header />
+        <Content />
+        <Footer />
+      </DataProvider>
+    </>
   );
 }
 
--- src/components/Content/Detail/default.js
+++ src/components/Content/Detail/default.js
@@ -2,41 +2,49 @@ import React from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import Track from "./Track";
 import { ReactComponent as Mic } from "../../../icons/mic.svg";
-
+import { Head } from "react-static";
 
 const DetailPodcast = ({ data }) => {
   const checkEpisodes = () => {
     return typeof data.episodes != "undefined";
   }
   return (
-    <div className="podcast-detail">
-      <div className="podcast-wrapper">
-        <div className="podcast-track-count">
-          <Mic /> {checkEpisodes() ? data.episodes.length : 0} Episode
-          {checkEpisodes() ? (data.episodes.length > 1 ? "s" : "") : ""}
+    <>
+      <Head>
+        <title>{data.title}</title>
+      </Head>
+      <div className="podcast-detail">
+        <div className="podcast-wrapper">
+          <div className="podcast-track-count">
+            <Mic /> {checkEpisodes() ? data.episodes.length : 0} Episode
+            {checkEpisodes() ? (data.episodes.length > 1 ? "s" : "") : ""}
+          </div>
+          <LazyLoadImage effect="blur" src={data.thumbnail} alt={data.title} />
+          <div className="podcast-info">
+            <h1>{data.title}</h1>
+            <small>
+              <a href={data.url} className="url-link">
+                {data.url}
+              </a>
+            </small>
+          </div>
         </div>
-        <LazyLoadImage effect="blur" src={data.thumbnail} alt={data.title} />
-        <div className="podcast-info">
-          <h1>{data.title}</h1>
-          <small>
-            <a href={data.url} className="url-link">
-              {data.url}
-            </a>
-          </small>
+        <div className="podcast-track-list">
+          <div className="podcast-tracks">
+            {checkEpisodes() ? (
+              data.episodes.map((episode, i) => (
+                <div className="podcast-track" key={i}>
+                  <h3>{`Episode-${episode.id}`}</h3>
+                  <Track url={episode.audio} />
+                </div>
+              ))
+            ) : (
+              <div className="no-track">No Podcast Found</div>
+            )}
+          </div>
         </div>
       </div>
-      <div className="podcast-track-list">
-        <div className="podcast-tracks">
-          {checkEpisodes() ? (
-            data.episodes.map((episodes, i) => (
-              <Track url={episodes.audio} key={i} />
-            ))
-          ) : (
-            <div className="no-track">No Podcast Found</div>
-          )}
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
